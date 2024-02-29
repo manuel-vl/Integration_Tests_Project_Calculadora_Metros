8b382cc6cabd4d96fdfa649a2c9c5616ccafa654
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-29 00:11:49 -0500

    [FEATURE] Add Test roomWithMoshWidthAndHeightIsBiggets and verifySquareMetersByRoom

--- calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/service/CalculateServiceTest.java
+++ calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/service/CalculateServiceTest.java
@@ -2,6 +2,7 @@ package com.mercadolibre.calculadorametroscuadrados.service;
 
 import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
 import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.RoomDTO;
 import com.mercadolibre.calculadorametroscuadrados.utils.TestUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,5 +30,39 @@ class CalculateServiceTest {
         Assertions.assertEquals(10400, houseResult.getPrice());
     }
 
+    @Test
+    void roomWithMostWithAndHeightIsBiggets(){
+        // Arrange
+        HouseDTO house=TestUtils.getHouseDTO();
+        RoomDTO room=new RoomDTO();
+
+        room.setName("room2");
+        room.setWidth(3);
+        room.setLength(3);
+
+        // Act
+        HouseResponseDTO houseResult=service.calculate(house);
+
+        // Assert
+        Assertions.assertEquals(room.getLength(), houseResult.getBiggest().getLength());
+        Assertions.assertEquals(room.getWidth(), houseResult.getBiggest().getWidth());
+    }
+
+    @Test
+    void verifySquareMetersByRoom(){
+        // Arrange
+        HouseDTO house=TestUtils.getHouseDTO();
+        RoomDTO room=new RoomDTO();
+
+        room.setName("room1");
+        room.setWidth(2);
+        room.setLength(2);
+
+        // Act
+        HouseResponseDTO houseResult=service.calculate(house);
+
+        // Assert
+        Assertions.assertEquals(room.getSquareFeet(), houseResult.getRooms().get(0).getSquareFeet());
+    }
 
 }
